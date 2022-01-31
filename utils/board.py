1b0b03ccc374da183829aaf36dc6f8dd26071821
Ronnrr <13minecraft2@gmail.com>
2022-01-31 23:36:53 +0200

    Setting up the base classes for the project.

---
+++ utils/board.py
@@ -0,0 +1,5 @@
+class Board:
+    def __init__(self):
+        self.planet_array = []
+        self.gui_array = []
+        self.frame_actions = []
---
+++ utils/planet.py
@@ -0,0 +1,5 @@
+class Planet:
+    def __init__(self, x, y, owner='0'):
+        self.x = x
+        self.y = y
+        self.owner = owner
